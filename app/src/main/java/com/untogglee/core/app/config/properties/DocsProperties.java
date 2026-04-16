57fd446fb387756120345bb8a095b37568a8f466
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2026-04-16 12:47:34 -0300

    refactor: removes spring project

--- app/src/main/java/com/untogglee/core/app/config/properties/DocsProperties.java
+++
@@ -1,9 +0,0 @@
-package com.untogglee.core.app.config.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "common.docs")
-public record DocsProperties(
-        String serverUrl
-) {
-}
